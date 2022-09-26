f5b9a6d6ab5eb1f382bf205c46a038749ec0115c
Swarna Sarker <swarnaprony81@gmail.com>
2022-09-26 16:00:46 +0200

    Update App.jsx in keep-ui and app.js in keep-api to fetch data from api into ui using useEffect fetch and cors

--- keep-ui/src/components/App.jsx
+++ keep-ui/src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState ,useEffect} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
@@ -7,32 +7,36 @@ import CreateArea from "./CreateArea";
 function App() {
   const [notes, setNotes] = useState([]);
 
-  function addNote(newNote) {
-    setNotes(prevNotes => {
-      return [...prevNotes, newNote];
-    });
-  }
+  useEffect(() => {
+    fetch("http://localhost:9000/notes")
+    .then(res => res.json())
+    .then(data => setNotes(data))
+  },[])
 
-  function deleteNote(id) {
-    setNotes(prevNotes => {
-      return prevNotes.filter((noteItem, index) => {
-        return index !== id;
-      });
-    });
-  }
+//   return(
+//     <div>
+//       <h1>{notes.title}</h1>
+//       <p>{notes.content}</p>
+//     </div>
+//   );
+// }
+
+  // function deleteNote(id) {
+  //   setNotes(prevNotes => {
+  //     return prevNotes.filter((noteItem, index) => {
+  //       return index !== id;
+  //     });
+  //   });
+  // }
 
   return (
     <div>
       <Header />
-      <CreateArea onAdd={addNote} />
       {notes.map((noteItem, index) => {
         return (
           <Note
-            key={index}
-            id={index}
             title={noteItem.title}
             content={noteItem.content}
-            onDelete={deleteNote}
           />
         );
       })}
